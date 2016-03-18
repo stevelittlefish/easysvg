3025ca9ee370d943ebcfc89f170cc132619b4f5a
Stephen Brown <steve@fig14.com>
2016-03-18 21:09:19 +0000

    more comment changes

--- __init__.py
+++ __init__.py
@@ -6,7 +6,9 @@ Simple package for drawing SVG images
 Library URL: git@lemon.com:pylib/svglib
 Check it out with: git subtree add --prefix ???/svglib git@lemon.com:pylib/svglib master
 Push it with: git subtree push --prefix=???/svglib git@lemon.com:pylib/svglib master
+Pull it with: git subtree pull --prefix=???/svglib git@lemon.com:pylib/svglib master
 
+Please pull this before modifying, and don't forget to push again afterwards!
 """
 
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
