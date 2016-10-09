8914caac9de4739840e48a986c142504985ed12a
Stephen Brown <steve@fig14.com>
2016-10-09 10:58:58 +0100

    Merged from Express Pharmacy - updated to python 3

--- __init__.py
+++ __init__.py
@@ -4,13 +4,8 @@ Simple package for drawing SVG images
 ---
 
 Library URL: git@lemon.com:pylib/svglib
-Check it out with: git subtree add --prefix ???/svglib git@lemon.com:pylib/svglib master
-Push it with: git subtree push --prefix=???/svglib git@lemon.com:pylib/svglib master
-Pull it with: git subtree pull --prefix=???/svglib git@lemon.com:pylib/svglib master
-
-Please pull this before modifying, and don't forget to push again afterwards!
 """
 
-__author__ = 'Stephen Brown (Little Fish Solutions LTD)'
+from .svgrender import SvgGenerator, SvgPathGenerator, rotation_transform
 
-from svgrender import SvgGenerator, SvgPathGenerator, rotation_transform
+__author__ = 'Stephen Brown (Little Fish Solutions LTD)'
