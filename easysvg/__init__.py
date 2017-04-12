fd21532147ef29df2dba5b3ca54ce537b44bae62
Stephen Brown <steve@fig14.com>
2017-04-12 09:34:59 +0100

    Removed version.py, bumped version number

--- easysvg/__init__.py
+++ easysvg/__init__.py
@@ -7,6 +7,5 @@ Library URL: git@lemon.com:pylib/svglib
 """
 
 from .svgrender import SvgGenerator, SvgPathGenerator, rotation_transform, render_error_svg
-from .version import VERSION
 
 __author__ = 'Stephen Brown (Little Fish Solutions LTD)'
--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='easysvg',
     packages=['easysvg'],
-    version='0.2',
+    version='0.2.1',
     description='A simple Python SVG library',
     author='Stephen Brown (Little Fish Solutions LTD)',
     author_email='opensource@littlefish.solutions',
     url='https://github.com/stevelittlefish/easysvg',
-    download_url='https://github.com/stevelittlefish/easysvg/archive/0.2.tar.gz',
+    download_url='https://github.com/stevelittlefish/easysvg/archive/v0.2.1.tar.gz',
     keywords=['svg', 'simple'],
     classifiers=[],
 )
