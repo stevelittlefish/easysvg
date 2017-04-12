7ca7ac74b4a340d8668934da96c203bd05d4ced3
Stephen Brown <steve@fig14.com>
2017-04-12 09:13:07 +0100

    Version number

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='easysvg',
     packages=['easysvg'],
-    version='0.1',
+    version='0.2',
     description='A simple Python SVG library',
     author='Stephen Brown (Little Fish Solutions LTD)',
     author_email='opensource@littlefish.solutions',
     url='https://github.com/stevelittlefish/easysvg',
-    download_url='https://github.com/stevelittlefish/easysvg/archive/0.1.tar.gz',
+    download_url='https://github.com/stevelittlefish/easysvg/archive/0.2.tar.gz',
     keywords=['svg', 'simple'],
     classifiers=[],
 )
