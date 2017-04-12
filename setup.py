f404b85c94836d6300230f7ca99a662761bf29c7
Stephen Brown <steve@fig14.com>
2017-04-12 09:12:13 +0100

    PyPi setup stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+    name='easysvg',
+    packages=['easysvg'],
+    version='0.1',
+    description='A simple Python SVG library',
+    author='Stephen Brown (Little Fish Solutions LTD)',
+    author_email='opensource@littlefish.solutions',
+    url='https://github.com/stevelittlefish/easysvg',
+    download_url='https://github.com/stevelittlefish/easysvg/archive/0.1.tar.gz',
+    keywords=['svg', 'simple'],
+    classifiers=[],
+)
+
